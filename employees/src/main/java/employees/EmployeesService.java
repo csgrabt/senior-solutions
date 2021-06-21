11400fd0fce2890a4de880e89794d210b14c95b5
Csekő György <gy.cseko@outlook.com>
2021-06-21 18:21:20 +0200

    api

--- employees/src/main/java/employees/EmployeesService.java
+++ employees/src/main/java/employees/EmployeesService.java
@@ -4,22 +4,22 @@ package employees;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.stereotype.Service;
+
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @Service
 public class EmployeesService {
     private ModelMapper modelMapper;
-
-
-
     private List<Employee> employeeList = Collections.synchronizedList(new ArrayList<>(
             List.of(
                     new Employee(1L, "John Doe"),
-                    new Employee(1L, "Jack Doe")
+                    new Employee(2L, "Jack Doe")
             )
     ));
 
@@ -28,9 +28,23 @@ public class EmployeesService {
 
     }
 
-     public List<EmployeeDto> employeeList() {
-        Type targetListType = new TypeToken<List<EmployeeDto>>(){}.getType();
-        return modelMapper.map(employeeList, targetListType);
-     }
+    public List<EmployeeDto> employeeList(Optional<String> prefix) {
+        Type targetListType = new TypeToken<List<EmployeeDto>>() {
+        }.getType();
+        List<Employee> filtered = employeeList
+                .stream()
+                .filter(n -> prefix.isEmpty() || n.getName().toLowerCase().startsWith(prefix.get().toLowerCase()))
+                .collect(Collectors.toList());
 
+        return modelMapper.map(filtered, targetListType);
+    }
+
+    public EmployeeDto findEmployeeById(long id) {
+        return modelMapper.map(employeeList
+                        .stream()
+                        .filter(n -> n.getId() == id)
+                        .findAny()
+                        .orElseThrow(() -> new IllegalArgumentException("Employee not found: " + id)),
+                EmployeeDto.class);
+    }
 }
