473f6d0ad550b90230e849d72d9f0a6237142043
Csekő György <gy.cseko@outlook.com>
2021-06-22 15:56:16 +0200

    Üdvözlés az oldalon

---
+++ car/src/main/java/car/HelloService.java
@@ -0,0 +1,14 @@
+package car;
+
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class HelloService {
+
+    public String sayHello() {
+        return "Üdvözlünk az oldalon";
+    }
+
+}
