17a5d62462aa96996f955a56e068f97e45647b13
Csekő György <gy.cseko@outlook.com>
2021-06-29 15:48:00 +0200

    First Commit

---
+++ movie2_0/src/main/java/com/example/movie2_0/Movie20Application.java
@@ -0,0 +1,13 @@
+package com.example.movie2_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Movie20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Movie20Application.class, args);
+    }
+
+}
---
+++ movie2_0/src/test/java/com/example/movie2_0/Movie20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movie2_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Movie20ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
