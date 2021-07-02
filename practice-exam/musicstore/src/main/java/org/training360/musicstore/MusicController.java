2f61244ee5330ee84c91911602109446637c1afb
Csekő György <gy.cseko@outlook.com>
2021-07-02 13:34:07 +0200

    Keresés Id alapján

--- practice-exam/musicstore/src/main/java/org/training360/musicstore/MusicController.java
+++ practice-exam/musicstore/src/main/java/org/training360/musicstore/MusicController.java
@@ -1,10 +1,15 @@
 package org.training360.musicstore;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.zalando.problem.Problem;
+import org.zalando.problem.Status;
 
 import javax.validation.Valid;
 import java.awt.event.WindowStateListener;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -38,4 +43,23 @@ public class MusicController {
     public InstrumentDTO findEmployeeById(@PathVariable("id") long id) {
         return musicStoreServie.findInstrumentById(id);
     }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Problem> handleNotFound(IllegalArgumentException iae) {
+        Problem problem =
+                Problem.builder()
+                        .withType(URI.create("instruments/not-found"))
+                        .withTitle("Not found")
+                        .withStatus(Status.NOT_FOUND)
+                        .withDetail(iae.getMessage())
+                        .build();
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_PROBLEM_JSON)
+                .body(problem);
+
+    }
+
+
 }
