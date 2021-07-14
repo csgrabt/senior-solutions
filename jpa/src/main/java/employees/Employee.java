2e68ce62a86be7368f1854514087e28dc23ac270
Csekő György <gy.cseko@outlook.com>
2021-07-14 09:32:38 +0200

    áttértem Lombok használatára

--- jpa/src/main/java/employees/Employee.java
+++ jpa/src/main/java/employees/Employee.java
@@ -1,8 +1,16 @@
 package employees;
 
 
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "employees")
 public class Employee {
@@ -14,34 +22,9 @@ public class Employee {
     @Column(name = "emp_name", length = 200, nullable = false)
     private String name;
 
-    public Employee() {
-    }
-
     public Employee(String name) {
         this.name = name;
     }
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
