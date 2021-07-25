9cee841bd402e6bcab673f84666f4362bf36a67a
Csekő György <gy.cseko@outlook.com>
2021-07-25 11:06:23 +0200

    First commit, location-spring-boot alkalmazás újra felépíése nulláról.

---
+++ location-spring-solution-again/src/main/java/com/example/locationspringsolutionagain/LocationSpringSolutionAgainApplication.java
@@ -0,0 +1,13 @@
+package com.example.locationspringsolutionagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationSpringSolutionAgainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationSpringSolutionAgainApplication.class, args);
+    }
+
+}
