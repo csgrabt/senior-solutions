8a5e33aa63b3e76569ce915d86f091cd474710a2
Csekő György <gy.cseko@outlook.com>
2021-07-25 11:38:47 +0200

    Bean annotáció kiprobálása a serive osztályról lekerült a @Service annotáció, és egy @Bean annotációval ellátott metódus példányosítja.

--- location-spring-solution-again/src/main/java/com/example/locationspringsolutionagain/LocationSpringSolutionAgainApplication.java
+++ location-spring-solution-again/src/main/java/com/example/locationspringsolutionagain/LocationSpringSolutionAgainApplication.java
@@ -2,6 +2,7 @@ package com.example.locationspringsolutionagain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LocationSpringSolutionAgainApplication {
@@ -10,4 +11,8 @@ public class LocationSpringSolutionAgainApplication {
         SpringApplication.run(LocationSpringSolutionAgainApplication.class, args);
     }
 
+   // @Bean
+   // public LocationService locationService() {
+   //     return new LocationService();
+   // }
 }
